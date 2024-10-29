679bbac98d9f40eac19a8c8ec8f10c0426f2fa08
Sanjoy123Parui <joy78sp@gmail.com>
2024-10-29 19:09:06 +0530

    Some issue in to the server is committed

--- server/src/connections/socketconnection.js
+++ server/src/connections/socketconnection.js
@@ -4,11 +4,6 @@ import express from "express";
 import { corsOption } from '../lib/optionconfig.js';
 import cookieParser from "cookie-parser";
 import { Server } from "socket.io";
-import {
-    chatSeeders
-} from '../seeders/chat.namespace.js';
-import { socketIoAuthenticator } from '../middlewares/auth.middleware.js';
-import { checkEventsError } from '../middlewares/errors.middleware.js';
 
 
 // here define object of socket.io connection
@@ -16,44 +11,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: corsOption
-});
+}); 
 
-
-// here was declare user socket ids
-const socketUser = new Map();
-
-
-// here are use socket.io authentication of middleware
-io.use((socket, next) => {
-
-    cookieParser()(
-        socket.request,
-        socket.request.res,
-        async (err) => await socketIoAuthenticator(err, socket, next)
-    );
-
-})
-
-
-// here define all namespace
-chatSeeders();
-
-
-// here was use global error middleware
-io.use(async (socket, next) => {
-
-    try {
-
-        next();
-
-    }
-    catch (err) {
-        await checkEventsError(err, socket, next);
-    }
-
-});
 
 
 // export socket.io connection
-export { express, cookieParser, io, socketUser, app, server };
+export { express, cookieParser, io, app, server };
 console.log("Websocket are worked successfully");
