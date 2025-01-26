8b9fb2e04f8290c5090ebb135e5c7a04a821be1f
Sanjoy123Parui <joy78sp@gmail.com>
2025-01-26 12:18:25 +0530

    issue in ui of client side code so i will fixed is are my committed

--- client/src/components/users/chats.tsx
+++ client/src/components/users/chats.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import ListContent from './contents/listContent.tsx';
 
 // here define Chats functional components
 const Chats: React.FC = () => {
@@ -10,21 +9,7 @@ const Chats: React.FC = () => {
 
     return (
         <>
-            {/* start div flex */}
-            <div className="flex flex-col md:flex-row mx-auto h-screen">
-
-                {/* ChatList */}
-                <div className="md:w-1/3 w-full border-[1px] border-gray-300 p-4 shadow-md h-screen">
-                    <ListContent />
-                </div>
-
-                {/* Chat Messages */}
-                <div className="hidden md:block md:w-2/3 w-full border-[1px] border-gray-300 p-4 h-screen shadow-md">
-                    <h1 className="text-lg font-semibold">Chat Messages</h1>
-                </div>
-
-            </div>
-            {/* end div flex */}
+            <h1>Chats</h1>
         </>
     );
 }
