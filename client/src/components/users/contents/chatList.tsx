ffb4f7ca9cee5af874bd5af027098b7a8f09a4cf
Sanjoy123Parui <joy78sp@gmail.com>
2025-02-15 22:09:27 +0530

    modified the client side code

--- client/src/components/users/contents/chatList.tsx
+++ client/src/components/users/contents/chatList.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarImage } from "../../ui/avatar.tsx";
 import ListHeading from "./listHeading.tsx";
 
 // here define chat list functional component
@@ -10,7 +11,34 @@ const ChatList: React.FC = () => {
 
       {/* start div for chat list content */}
       <div className="border-t px-2">
-        <h1>Chat List</h1>
+        {/* start ul of user list */}
+        <ul className="max-w-lg divide-y divide-gray-200 dark:divide-gray-700">
+          <li className="py-4 lg:py-3">
+            <div className="flex items-center space-x-4 rtl:space-x-reverse">
+              <div className="shrink-0">
+                <Avatar>
+                  <AvatarImage
+                    src=""
+                    className="w-full h-full rounded-full"
+                    alt="avatar image"
+                  />
+                </Avatar>
+              </div>
+              <div className="flex-1 min-w-0">
+                <p className="text-sm md:text-base font-medium truncate dark:text-white">
+                  Neil sims
+                </p>
+                <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                  Hii
+                </p>
+              </div>
+              <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white px-2">
+                1
+              </div>
+            </div>
+          </li>
+        </ul>
+        {/* end ul */}
       </div>
       {/* end div */}
     </>
