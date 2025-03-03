8fe83f33839e77a1173415ece8bec7dd55a4b432
Sanjoy123Parui <joy78sp@gmail.com>
2025-03-03 22:47:06 +0530

    New file create middleware of server code

--- server/server.js
+++ server/server.js
@@ -25,8 +25,6 @@ import { contactRouter } from "./src/routes/contact.route.js";
 import { adminSignupRouter } from "./src/routes/adminSignup.route.js";
 import { adminProfileRouter } from "./src/routes/adminProfile.route.js";
 import { adminDashboardRouter } from "./src/routes/adminDashboard.route.js";
-// import all namespace
-import { chatsContent } from "./src/seeders/chats.js";
 // import database path
 import { databaseConnection } from "./src/config/conncectdb.js";
 
@@ -66,9 +64,6 @@ if (cluster.isPrimary) {
   app.use("/admin/api", adminSignupRouter);
   app.use("/admin/api", adminProfileRouter);
   app.use("/admin/dashboard/api", adminDashboardRouter);
-
-  // here was namsepace events functions call
-  chatsContent();
 
   // use error middlewares
   app.use(checkError);
