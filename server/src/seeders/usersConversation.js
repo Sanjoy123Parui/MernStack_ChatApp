92b0c11ab7732b4a780372901decb12630769c35
Sanjoy123Parui <joy78sp@gmail.com>
2025-03-09 21:21:36 +0530

    modified server side code file and create new file are committed successfully

---
+++ server/src/seeders/usersConversation.js
@@ -0,0 +1,32 @@
+// here import some libraries
+import { io, userSocketIds } from "../connections/socketconnection.js";
+import { userSocketAuth } from "../middlewares/socketAuth.middleware.js";
+
+// here was conversation funtions are define for namespace
+export const socketUsers = async () => {
+  try {
+    // here was define usernamespce
+    const usersIo = io.of("/users-namespace");
+
+    // here was declare socket event authentication middleware use
+    usersIo.use(userSocketAuth);
+
+    // here define connection of namespace
+    await usersIo.on("connection", (socket) => {
+      // here was retrive userId
+      let userId = socket.user;
+      // here can set userId with socketId
+      userSocketIds.set(userId.toString(), socket.id);
+      console.log("User connection successfully", userSocketIds);
+
+      // here define disconnect socket of namespace
+      socket.on("disconnect", () => {
+        // here remove userId of sockets when disconnect
+        userSocketIds.delete(userId.toString());
+        console.log("User disconnected", userSocketIds);
+      });
+    });
+  } catch (error) {
+    console.log(`Socket error occurred:${error}`);
+  }
+};
