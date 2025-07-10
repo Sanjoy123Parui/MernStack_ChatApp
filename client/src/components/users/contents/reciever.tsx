a73607058d0c38af9cb7105167af641f381819a8
Sanjoy123Parui <joy78sp@gmail.com>
2025-07-10 21:04:36 +0530

    Today my commitment is in client side code sender and reciever preview images content ui task are completed successfully

--- client/src/components/users/contents/reciever.tsx
+++ client/src/components/users/contents/reciever.tsx
@@ -1,26 +1,27 @@
 // import { BsPlayFill } from "react-icons/bs";
 // import { LuAudioLines } from "react-icons/lu";
+// import { MdOutlineFileDownload } from "react-icons/md";
 
 // RecieverTextMessage component
-// const RecieverTextMessage: React.FC = () => {
-//   return (
-//     <>
-//       <div className="bg-white text-black p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
-//         {/* reciever text-message name or phone */}
-//         <h2 className="text-sm md:text-lg font-medium">You</h2>
+const RecieverTextMessage: React.FC = () => {
+  return (
+    <>
+      <div className="bg-white text-black p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
+        {/* reciever text-message name or phone */}
+        <h2 className="text-sm md:text-lg font-medium">You</h2>
 
-//         {/* reciever text messages messages */}
-//         <p className="font-normal text-sm md:text-base py-1">Hi</p>
+        {/* reciever text messages messages */}
+        <p className="font-normal text-sm md:text-base py-1">Hi</p>
 
-//         {/* start reciever text message dilivery time */}
-//         <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
-//           <span className="text-xs md:text-sm font-medium">11:46 PM</span>
-//         </div>
-//         {/* end reciever text message dilivery time */}
-//       </div>
-//     </>
-//   );
-// };
+        {/* start reciever text message dilivery time */}
+        <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
+          <span className="text-xs md:text-sm font-medium">11:46 PM</span>
+        </div>
+        {/* end reciever text message dilivery time */}
+      </div>
+    </>
+  );
+};
 
 // RecieverVoiceMessage component
 // const RecieverVoiceMessage: React.FC = () => {
@@ -65,13 +66,44 @@
 
 
 // RecieverImage component
-const RecieverImage: React.FC = () => {
-  return (
-    <>
-      <h1>Hello Reciever</h1>
-    </>
-  );
-};
+// const RecieverImage: React.FC = () => {
+//   return (
+//     <>
+//       <div className="bg-white text-black p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
+//         {/* sender phone or name */}
+//         <h2 className="text-sm md:text-lg font-medium">You</h2>
+
+//         {/* start preview image content */}
+//         <div className="group relative my-2.5">
+
+//           {/* start download button content */}
+//           <div className="absolute inset-0 bg-gray-900/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 
+//           rounded-lg flex items-center justify-center">
+//             <a
+//               aria-label="Download image"
+//               className="inline-flex items-center justify-center rounded-full h-8 w-8 md:h-10 md:w-10 bg-white/30 hover:bg-white/50 focus:ring-4 focus:outline-none focus:ring-gray-50"
+//             >
+//               <MdOutlineFileDownload className="w-4 h-4 md:w-5 md:h-5 text-white" />
+//             </a>
+//           </div>
+//           {/* end download button content */}
+
+//           <img src="" alt="Reciever images" className="w-full h-auto max-h-80 sm:max-h-72 object-contain " />
+//         </div>
+//         {/* end preview image content */}
+
+//         {/*  reciever caption  */}
+//         <p className="mt-2 text-xs md:text-sm font-normal break-words">Hii friends how are look like from me</p>
+
+//         {/* start reciever text message dilivery time */}
+//         <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
+//           <span className="text-xs md:text-sm font-medium">11:46 PM</span>
+//         </div>
+//         {/* end reciever text message dilivery time */}
+//       </div>
+//     </>
+//   );
+// };
 
 
 
@@ -80,8 +112,8 @@ const Reciever: React.FC = () => {
   return (
     <>
       <div className="flex items-start justify-start">
-        <RecieverImage />
-        {/* <RecieverTextMessage /> */}
+        {/* <RecieverImage /> */}
+        <RecieverTextMessage />
         {/* <RecieverVoiceMessage /> */}
       </div>
     </>
--- client/src/components/users/contents/sender.tsx
+++ client/src/components/users/contents/sender.tsx
@@ -2,34 +2,36 @@ import {
   // BiCheck,
   BiCheckDouble,
 } from "react-icons/bi";
+// import { MdOutlineFileDownload } from "react-icons/md";
 // import { BsPlayFill } from "react-icons/bs";
 // import { LuAudioLines } from "react-icons/lu";
+// import {Profilepic} from "../../../../public/images/profilePic.jpg";
 
 // SenderTextMessage component
-// const SenderTextMessage: React.FC = () => {
-//   return (
-//     <>
-//       <div className="bg-indigo-500 text-white p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
-//         <h2 className="text-sm md:text-lg font-medium">You</h2>
-//         {/* text messages */}
-//         <p className="font-normal text-sm md:text-base py-1">
-//           Hi Everyone today I will go to the NJP, So Have you any can meet me in
-//           Station
-//         </p>
+const SenderTextMessage: React.FC = () => {
+  return (
+    <>
+      <div className="bg-indigo-500 text-white p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
+        {/*Sender phone or name */}
+        <h2 className="text-sm md:text-lg font-medium">You</h2>
+        {/* text messages */}
+        <p className="font-normal text-sm md:text-base py-1">
+          Hi Everyone today I will go to the NJP, So Have you any can meet me in
+          Station
+        </p>
 
-//         {/* start sender text message dilivery time */}
-//         <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
-//           <span className="text-xs md:text-sm font-medium">11:46 PM</span>
-//           {/* <span className="text-sm font-normal"><BiCheck /></span> */}
-//           <span className="text-sm md:text-lg font-medium">
-//             <BiCheckDouble />
-//           </span>
-//         </div>
-//         {/* end sender text message dilivery time */}
-//       </div>
-//     </>
-//   );
-// };
+        {/* start sender text message dilivery time */}
+        <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
+          <span className="text-xs md:text-sm font-medium">11:46 PM</span>
+          <span className="text-sm md:text-lg font-medium">
+            <BiCheckDouble />
+          </span>
+        </div>
+        {/* end sender text message dilivery time */}
+      </div>
+    </>
+  );
+};
 
 
 
@@ -38,6 +40,7 @@ import {
 //   return (
 //     <>
 //       <div className="bg-indigo-500 text-white p-4 border shadow-md rounded-lg max-w-60 lg:max-w-96">
+//        {/*Sender phone or name */}
 //         <h1 className="text-sm md:text-lg font-medium">You</h1>
 //         {/* start voice message content */}
 //         <div className="flex items-center space-x-2 py-1">
@@ -63,6 +66,7 @@ import {
 //         {/* delivery content start */}
 //         <div className="flex items-center justify-end w-full space-x-2 rtl:space-x-reverse">
 //           <span className="text-xs md:text-sm font-normal">07:35 PM</span>
+//           {/* <span className="text-sm font-normal"><BiCheck /></span> */ }
 //           <span className="text-sm md:text-lg font-normal">
 //             <BiCheckDouble />
 //           </span>
@@ -76,13 +80,39 @@ import {
 
 
 //SenderImage component
-const SenderImage: React.FC = () => {
-  return (
-    <>
-      <h1>Hello Image</h1>
-    </>
-  );
-}
+// const SenderImage: React.FC = () => {
+//   return (
+//     <>
+//       <div className="bg-indigo-500 text-white p-4 border shadow-md rounded-lg max-w-60 md:max-w-96">
+//         {/* sender phone or name */}
+//         <h2 className="text-sm md:text-lg font-medium">You</h2>
+
+//         {/* start image preview content*/}
+//         {/* <div className="group relative my-2.5">
+//           <img
+//             src=""
+//             alt="Sent image"
+//             className="w-full h-auto max-h-80 sm:max-h-72 object-contain rounded-lg"
+//           />
+//         </div> */}
+//         {/* end image preview content */}
+
+//         {/* caption content of sender */}
+//         <p className="mt-2 text-xs md:text-sm font-normal break-words">Hii friends how are look like from me</p>
+
+//         {/* start sender image delivery time */}
+//         <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
+//           <span className="text-xs md:text-sm font-medium">11:46 PM</span>
+//           <span className="text-sm md:text-lg font-medium">
+//             <BiCheckDouble />
+//           </span>
+//         </div>
+//         {/* end sender image delivery time */}
+//       </div>
+//     </>
+//   );
+// };
+
 
 // here define Sender component for content of sender
 const Sender: React.FC = () => {
@@ -90,8 +120,8 @@ const Sender: React.FC = () => {
     <>
       {/* start div flex for sender all chat content */}
       <div className="flex items-end justify-end">
-        <SenderImage />
-        {/* <SenderTextMessage /> */}
+        {/* <SenderImage /> */}
+        <SenderTextMessage />
         {/* <SenderVoiceMessage /> */}
       </div>
       {/* end div */}
