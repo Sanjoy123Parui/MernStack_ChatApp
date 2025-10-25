d8ae34079867f5d5872cc9dd36f1f2eea71a1d0a
Sanjoy123Parui <joy78sp@gmail.com>
2025-10-25 20:52:46 +0530

    Today task was complete as sender and reciver social media ui content in client side code are committed successfully

--- client/src/components/users/contents/sender.tsx
+++ client/src/components/users/contents/sender.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 import {
   // BiCheck,
   BiCheckDouble,
@@ -708,6 +707,102 @@ const SenderTextMessage: React.FC<any> = ({ senderdropDownMenu }) => {
 //   );
 // };
 
+/* SenderSocialMedia component */
+// const SenderSocialMedia: React.FC<any> = ({ senderdropDownMenu }) => {
+//   const [senderSocialMediaMenu, setSenderSocialMediaMenu] =
+//     useState<boolean>(false);
+
+//   const url: any = `http://localhost:5173/user/content/story`;
+//   const title: string = `Nature Video | 2025 | London`;
+//   const desc: string = `#nature #ntl #video #gimmie #youtube`;
+
+//   const handleSenderSocialMediaMenu = (): any =>
+//     setSenderSocialMediaMenu((prev: any) => !prev);
+
+//   return (
+//     <>
+//       <div className="flex items-start gap-2.5">
+//         <div className="w-10 h-10 overflow-hidden border-white border-[2px] font-bold bg-gray-100 rounded-full dark:bg-gray-700">
+//           <FaUser className="w-8 h-8 text-gray-400 p-[2px] mt-1 mx-auto" />
+//         </div>
+
+//         <div className="relative bg-indigo-500 text-white p-4 border shadow-md rounded-lg max-w-72 sm:max-w-80 md:max-w-72 w-full">
+//           {/* sender socialmedia phone or name, menu button content */}
+//           <div className="flex items-center justify-between">
+//             <h2 className="text-sm md:text-base font-medium">You</h2>
+//             <button
+//               type="button"
+//               onClick={handleSenderSocialMediaMenu}
+//               className="p-1 rounded-full hover:bg-indigo-600"
+//             >
+//               <BsThreeDotsVertical className="w-5 h-5" />
+//             </button>
+//           </div>
+
+//           {/* sender socialmedia thumbnail content */}
+//           <div className="py-1">
+//             <div className="relative w-full flex justify-center">
+//               <video
+//                 src="../../src/assets/videos/nature.mp4"
+//                 className="w-full h-40 sm:h-48 md:h-56 object-cover rounded-lg border border-white"
+//                 controls={false}
+//                 muted
+//                 tabIndex={-1}
+//               />
+//               <button
+//                 type="button"
+//                 className="absolute inset-0 flex items-center
+//               justify-center focus:outline-none pointer-events-auto"
+//               >
+//                 <FaRegCirclePlay className="text-white w-16 h-16 bg-black bg-opacity-40 rounded-full p-2 transition-opacity duration-200 opacity-100" />
+//               </button>
+//             </div>
+
+//             {/* sender social media description, title and channel name content */}
+//             <div className="mt-3">
+//               <p className="text-sm md:text-base font-medium">{title}</p>
+//               <p className="text-sm text-indigo-100  line-clamp-3">{desc}</p>
+//             </div>
+//             {/* url content */}
+//             <a
+//               href={url}
+//               rel="_blank"
+//               target="noopener noreferrer"
+//               className="block mt-2 text-xs sm:text-sm md:text-base
+//             text-indigo-100 break-words"
+//             >
+//               {url}
+//             </a>
+//           </div>
+
+//           {/* sender delivery time zone */}
+//           <div className="flex items-center justify-end space-x-2 rtl:space-x-reverse">
+//             <span className="text-xs md:text-sm font-medium">10:51 AM</span>
+//             <span className="text-base font-medium">
+//               <BiCheckDouble />
+//             </span>
+//           </div>
+
+//           {/* sender social media dropdown menu */}
+//           {senderSocialMediaMenu && (
+//             <div className="absolute top-12 right-4 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+//               <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+//                 {senderdropDownMenu.map((item: any, index: any) => (
+//                   <li key={index}>
+//                     <a className="block px-4 py-2 hover:bg-gray-100 dark:bg-gray-600 dark:hover:text-white">
+//                       {item}
+//                     </a>
+//                   </li>
+//                 ))}
+//               </ul>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
 /* here define Sender component for content of sender*/
 const Sender: React.FC = () => {
   // here declare variables of dropdown menu list of sender
@@ -731,6 +826,7 @@ const Sender: React.FC = () => {
         {/* <SenderImagesGallery senderdropDownMenu={senderdropDownMenu} /> */}
         {/* <SenderVideosGallery senderdropDownMenu={senderdropDownMenu} /> */}
         {/* <SenderWebUrl senderdropDownMenu={senderdropDownMenu} /> */}
+        {/* <SenderSocialMedia senderdropDownMenu={senderdropDownMenu} /> */}
       </div>
       {/* end div */}
     </>
